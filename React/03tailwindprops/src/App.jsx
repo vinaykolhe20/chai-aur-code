f48cc86ba7e4ae15e02ee548c47001a25fc4e10a
vinaykolhe20 <vinaykolhe9183@gmail.com>
2024-03-08 09:26:44 +0530

    completed till 8

---
+++ React/03tailwindprops/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj={
+    name:'Vimal',
+    age:43
+  }
+  let newArr=[1,2,3]
+  return (
+    <>
+        <h1 className='bg-green-400 text-black mb-4 p-4 rounded-xl  '>Tailwind Test</h1>
+        <Card channel="chaiaurcode"/>
+    </>
+  )
+}
+
+export default App
